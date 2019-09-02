1746303e879c50fadd454540b3bcf3dc3ebd42ba
dev_seung2 <dev_seung2@naver.com>
2019-09-03 00:00:09 +0900

    백준 10610번

---
+++ BOJ/BOJ_10610.py
@@ -0,0 +1,18 @@
+# 백준 알고리즘 10610번 문제
+import sys
+
+N = input()
+
+N_list = []
+
+for i in N :
+    N_list.append(i)
+
+N_list.sort(reverse = True)
+
+str_max = "".join(N_list)
+
+if (int(str_max) % 30) == 0 :
+    print(str_max)
+else :
+    print(-1)
