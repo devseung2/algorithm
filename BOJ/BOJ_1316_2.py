c477f201e5328b14d7d5809d7e7b8af97df4f15d
dev_seung2 <dev_seung2@naver.com>
2019-09-21 00:59:00 +0900

    백준 1316번 문제

--- BOJ/BOJ_1316_2.py
+++ BOJ/BOJ_1316_2.py
@@ -11,10 +11,8 @@ _sum = 0
 for _str in arr : 
     check = True
     for i in range(1, len(_str)) :   
-        if _str[i] != _str[i-1] :
-            if _str[i+1:len(_str)].count(_str[i-1]) > 0 :
-                check = False
-        if not check :
+        if _str[i] != _str[i-1] and _str[i+1:len(_str)].count(_str[i-1]) > 0 :
+            check = False
             break
     if check : 
         _sum += 1
