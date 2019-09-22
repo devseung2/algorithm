151c568b65715c69879f481406824f3d19805f1e
dev_seung2 <dev_seung2@naver.com>
2019-09-23 02:12:17 +0900

    백준 11403번 DFS 문제

---
+++ BOJ/BOJ_11403DFS.py
@@ -0,0 +1,37 @@
+# 백준 11403번 문제
+import sys
+import copy
+
+N = int(sys.stdin.readline().strip())
+
+arr = []
+for i in range(N) :
+    arr.append(list(map(str, sys.stdin.readline().strip().split())))
+
+g = [[] for i in range(len(arr))]
+
+for i in range(len(arr)) :
+    for j in range(len(arr[0])) :
+        if arr[i][j] == "1" :
+            g[i].append(j)
+
+for i in range(len(g)) :
+    g_c = copy.deepcopy(g)
+    if not g_c[i] :
+        continue
+    stack = [i]
+    visited = []
+    while stack :
+        v = stack[len(stack)-1]
+        if g_c[v] :
+            p = g_c[v].pop()
+            if p not in visited :
+                stack.append(p)
+                visited.append(p)
+        else : 
+            stack.pop()
+    for val in visited :
+        arr[i][val] = "1"
+
+for arr_x in arr :
+    print(" ".join(arr_x))
