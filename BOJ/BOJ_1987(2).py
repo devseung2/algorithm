3d6b53ba31db2475adb468e81e3ccbe3f260c2df
dev_seung2 <dev_seung2@naver.com>
2019-10-12 03:53:06 +0900

    백준 1987번 문제

--- BOJ/BOJ_1987(2).py
+++ BOJ/BOJ_1987(2).py
@@ -3,12 +3,12 @@ R, C = map(int, input().split())
 arr = [list(str(input())) for __ in range(R)]
 visited = [[0 for _ in range(C)] for __ in range(R)]
 visited[0][0] = 1
-visited_Alpha = [arr[0][0]]
+visited_Alpha = [0 for _ in range(26)]
+visited_Alpha[ord(arr[0][0])-65] = 1
 stack = [[0,0]]
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 _max = 1
-
 while stack :
     now_r, now_c = stack[len(stack)-1]
     l = len(stack)
@@ -16,16 +16,16 @@ while stack :
         temp_r = now_r + dr[i]
         temp_c = now_c + dc[i]
 
-        if 0 <= temp_r < R and 0 <= temp_c < C and visited[temp_r][temp_c] == 0 and arr[temp_r][temp_c] not in visited_Alpha :
+        if 0 <= temp_r < R and 0 <= temp_c < C and visited[temp_r][temp_c] == 0 and visited_Alpha[ord(arr[temp_r][temp_c])-65] == 0 :
             stack.append([temp_r, temp_c])
-            visited_Alpha.append(arr[temp_r][temp_c])
             visited[now_r][now_c] = i+1
+            visited_Alpha[ord(arr[temp_r][temp_c])-65] = 1
             break
 
     if l == len(stack) :
         _max = max(_max, len(stack))
         visited[now_r][now_c] = 0
+        visited_Alpha[ord(arr[now_r][now_c])-65] = 0
         stack.pop()
-        visited_Alpha.pop()
         
 print(_max)
